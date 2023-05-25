b46f1e0080f22192cb82039ac16d8d0d3b9cb69c
Ladislav Zigo <zigo@profesia.sk>
2023-05-25 11:35:05 +0000

    first commit

---
+++ src/test/java/hello/world/HelloClientSpec.java
@@ -0,0 +1,18 @@
+package hello.world;
+
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
+
+@MicronautTest // (1)
+public class HelloClientSpec  {
+
+    @Inject
+    HelloClient client; // (2)
+
+    @Test
+    public void testHelloWorldResponse(){
+        assertEquals("Hello World", Mono.from(client.hello()).block());// (3)
+    }
+}
